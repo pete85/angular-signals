0d9317b2261f46ce5c3787cfef0879d4d0087866
Piotr Furmanek <info.pete85@gmail.com>
2024-09-11 17:25:20 +0100

    routing and nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 //import 'zone.js/dist/zone';  // Required for Stackblitz
-import { Component } from '@angular/core';
-import { RouterLinkActive, RouterLink, RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
 
 @Component({
   selector: 'app-root',
@@ -8,7 +13,13 @@ import { RouterLinkActive, RouterLink, RouterOutlet } from '@angular/router';
   imports: [
     RouterLinkActive,
     RouterLink,
-    RouterOutlet],
+    RouterOutlet,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
