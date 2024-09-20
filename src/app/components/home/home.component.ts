7a379be992a275373c2c764cb16cdad79233dc21
Piotr Furmanek <info.pete85@gmail.com>
2024-09-20 23:04:50 +0100

    homepage

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import {MatSlideToggle} from "@angular/material/slide-toggle";
 import {NgOptimizedImage} from "@angular/common";
+import {MatButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     MatSlideToggle,
-    NgOptimizedImage
+    NgOptimizedImage,
+    MatButton,
+    MatIcon
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
